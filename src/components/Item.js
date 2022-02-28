5b030f821277052a11d9c094c7bd897c3b4a7e8c
Taylor Huffman <taylor@crucialartists.com>
2022-02-28 15:00:19 -0600

    commit tree 0213adab97da998304819c755ae0fcf74d4a7331

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(true)
+  const cartItemClass = inCart ? "" : "in-cart"
+  const addToCartButtonClass = inCart ? "add" : "remove"
+
+  function addToCartHandler() {
+    setInCart((inCart) => !inCart)
+  }
+
   return (
-    <li className="">
+    <li className={cartItemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={addToCartButtonClass} onClick={addToCartHandler}>{inCart ? "Add to Cart" : "Remove From Cart"}</button>
     </li>
   );
 }
